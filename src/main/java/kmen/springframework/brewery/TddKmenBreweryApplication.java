e2fa5493df69bdd95475bd3fcde8331892a39012
Karol Mendza <karolmendza@gmail.com>
2019-08-28 13:58:36 +0200

    Initial commit

---
+++ src/main/java/kmen/springframework/brewery/TddKmenBreweryApplication.java
@@ -0,0 +1,13 @@
+package kmen.springframework.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddKmenBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddKmenBreweryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kmen/springframework/brewery/TddKmenBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package kmen.springframework.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddKmenBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
